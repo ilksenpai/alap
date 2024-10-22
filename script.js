ecc022acd42a6b91784be562b8daeaa2968651c3
ilksenpai <100223222+ilksenpai@users.noreply.github.com>
2024-10-22 15:31:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 function calculateFinances() {
-    let curr_sp = 18159.32; // current spent -- kredi kartı ekstresinde bugün için yazan miktar
-    let payment = 1746.65 + 5589.64 + 800.40; // taksit olarak ödenen miktarlar
-    let subs = 76.99 + 134.99 + 237.4 + 305.99; // abonelikler -- netflix, spotify vb gibi
-    let oneTime_sp = payment + subs; // taksit maksit (tek seferlik harcamalar)
-    let dayCount = 31; // o ay kaç çekiyosa o
-    let currDay = 22; // current day
-    let wage = 30000; // aylık gelir
-    let saveGoal = 5000; // aylık biriktirme hedefi
-    let debt = 450 + 200 + 300; // millete olan borçlar
-    let perc = 0.05; // birikim hesabı %si
+    let curr_sp = parseFloat(document.getElementById("curr_sp").value); // current spent
+    let payment = parseFloat(document.getElementById("payment").value); // payments
+    let subs = parseFloat(document.getElementById("subs").value); // subscriptions
+    let dayCount = parseFloat(document.getElementById("dayCount").value); // days in the month
+    let currDay = parseFloat(document.getElementById("currDay").value); // current day
+    let wage = parseFloat(document.getElementById("wage").value); // monthly income
+    let saveGoal = parseFloat(document.getElementById("saveGoal").value); // saving goal
+    let debt = parseFloat(document.getElementById("debt").value); // debts
+    let perc = parseFloat(document.getElementById("perc").value); // saving account percentage
+
+    let oneTime_sp = payment + subs;
 
     let max_sp = wage - saveGoal - debt;
-
     let recurr_sp = curr_sp - oneTime_sp;
-
     let totRecurr_sp = recurr_sp * dayCount / currDay;
-
     let tot_sp = totRecurr_sp + oneTime_sp + debt;
-
     let save = wage - tot_sp;
-
     let fac = save / saveGoal;
-
     let mustSave = perc * tot_sp;
-
     let save_sp = tot_sp + mustSave;
 
     document.getElementById("total_spending").innerHTML = `Total Spending Prediction: ${tot_sp.toFixed(2)} TL`;
